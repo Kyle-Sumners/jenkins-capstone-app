a2ad81a144b7c1c90bfe896bb154f8322efe2f1d
Kyle Sumners <ksumners@515tech.com>
2026-09-04 10:55:12 -0500

    add basic flask app and health check route

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return "<h1>Hello, World!</h1>"
+
+@app.route("/health", methods=['GET'])
+def get_health():
+  database_up = True
+
+  health_response = {
+    "status": "healthy",
+    "checks": {
+      "database": "up"
+    }
+  }
+
+  if not database_up:
+    health_response["status"] = "unhealthy"
+    health_response["checks"]["database"] = "down"
+  
+  return jsonify(health_response), (200 if health_response["status"] == "healthy" else 503) 
+
+if __name__ == "__main__":
+  app.run(debug=True)
