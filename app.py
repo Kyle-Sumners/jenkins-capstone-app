39758d701e19cfe38e9a61b26a8ba883bb95bc4f
Kyle Sumners <ksumners@515tech.com>
2026-09-09 08:33:16 -0500

    update home page content

--- app.py
+++ app.py
@@ -3,15 +3,27 @@ import os
 
 app = Flask(__name__)
 
+def current_version():
+    return os.getenv("VERSION", "0.0.0")
+
+def database_up():
+    return os.getenv("DB_STATUS", "up") == "up"
+
 @app.route("/")
 def home():
-  return f"<h1>Hello, World!</h1><br><h2>Version: {os.getenv('VERSION', '0.0.0')}"
+  status = "healthy" if database_up() else "unhealthy"
+
+  return f"""
+    <h1>Hello, World!</h1>
+    <p>Version: <strong>{current_version()}</strong></p>
+    <p>Status: {status}</p>
+  """
 
 @app.route("/health", methods=['GET'])
 def get_health():
-  database_up = os.getenv("DB_STATUS", "up") == "up"
-  version = os.getenv("VERSION", '0.0.0')
-
+  database_status = database_up()
+  version = current_version()
+  
   health_response = {
     "status": "healthy",
     "version": version,
@@ -20,7 +32,7 @@ def get_health():
     }
   }
 
-  if not database_up:
+  if not database_status:
     health_response["status"] = "unhealthy"
     health_response["checks"]["database"] = "down"
   
